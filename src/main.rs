4c54f8a7604ef890c803b22a6797360b0be1630f
Babbaj <babbaj45@gmail.com>
2023-03-02 07:36:30 -0500

    node-toggle

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-extern crate core;
-
 use std::{cell::Cell, ptr, rc::Rc};
+use std::collections::HashMap;
 use std::ffi::{c_char, c_int, c_uchar, c_ulong, CString};
 use std::io::Cursor;
 use std::time::Duration;
@@ -29,10 +28,29 @@ struct KeyboardListener {
     xi_opcode: c_int
 }
 
+#[derive(Debug)]
+struct Node {
+    global_id: u32,
+    proxy: pw::node::Node
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum KeyType {
+    HOLD,
+    TOGGLE
+}
+
+#[derive(Debug, PartialEq)]
+enum KeyEventType {
+    PRESS,
+    RELEASE
+}
+
 // keysym
-enum KeyEvent {
-    Press(c_ulong),
-    Release(c_ulong)
+#[derive(Debug)]
+struct KeyEvent {
+    type_: KeyEventType,
+    keysym: c_ulong
 }
 
 impl KeyboardListener {
@@ -55,16 +73,20 @@ impl KeyboardListener {
                         let keycode = event.detail;
 
                         let keysym = xlib::XKeycodeToKeysym(self.display, keycode as c_uchar, 0);
+                        let type_ = match cookie.evtype {
+                            xinput2::XI_KeyPress => KeyEventType::PRESS,
+                            xinput2::XI_KeyRelease => KeyEventType::RELEASE,
+                            _ => panic!()
+                        };
 
-                        return if cookie.evtype == xinput2::XI_KeyPress {
-                            KeyEvent::Press(keysym)
-                        } else {
-                            KeyEvent::Release(keysym)
+                        xlib::XFreeEventData(self.display, cookie as *mut _);
+                        return KeyEvent {
+                            type_,
+                            keysym,
                         }
                     }
-
-                    xlib::XFreeEventData(self.display, cookie as *mut _)
                 }
+                xlib::XFreeEventData(self.display, cookie as *mut _)
             }
         }
     }
@@ -126,14 +148,17 @@ fn name_to_keysym(name: &str) -> c_ulong {
 }
 
 fn parse_args() -> ArgMatches {
+    let node_arg = Arg::new("node")
+        .long("node")
+        .value_names(["NODE_NAME", "KEYSYM"])
+        .help("KEYSYM is x11 keysym name (the #define without \"XK_\")")
+        .action(ArgAction::Append);
+    let toggle_node_arg = node_arg.clone()
+        .id("node-toggle")
+        .long("node-toggle");
     let command = Command::new("multi_sink_source")
-        .arg(Arg::new("node")
-            .long("node")
-            .value_names(["NODE_NAME", "KEYSYM"])
-            .help("KEYSYM is x11 keysym name (the #define without \"XK_\")")
-            .required(true)
-            .action(ArgAction::Append)
-        )
+        .arg(node_arg)
+        .arg(toggle_node_arg)
         .arg(Arg::new("release-delay")
             .long("release-delay")
             .value_name("MILLIS")
@@ -166,6 +191,18 @@ fn create_mute_pod(mute: bool) -> Vec<u8> {
 static mut MUTE_POD: Vec<u8> = Vec::new();
 static mut UNMUTE_POD: Vec<u8> = Vec::new();
 
+fn node_args<'a>(args: &'a ArgMatches, id: &str, type_: KeyType) -> Vec<(&'a str, (KeyType, c_ulong))> {
+    if let Some(iter) = args.get_occurrences::<String>(id) {
+        iter.map(|mut it| (
+            it.next().unwrap().as_str(),
+            (type_, name_to_keysym(it.next().unwrap().as_str()))
+        ))
+            .collect()
+    } else {
+        Vec::new()
+    }
+}
+
 fn main() {
     unsafe {
         MUTE_POD = create_mute_pod(true);
@@ -173,12 +210,8 @@ fn main() {
     }
 
     let args = parse_args();
-    let pairs: Vec<(&str, c_ulong)> = args.get_occurrences::<String>("node").unwrap()
-        .map(|mut it| (
-            it.next().unwrap().as_str(),
-            name_to_keysym(it.next().unwrap().as_str())
-        ))
-        .collect();
+    let mut pairs = node_args(&args, "node", KeyType::HOLD);
+    pairs.extend(node_args(&args, "node-toggle", KeyType::TOGGLE));
     let release_delay = args.get_one::<String>("release-delay").unwrap().parse::<u64>()
         .expect("failed to parse release-delay");
 
@@ -197,27 +230,36 @@ fn main() {
     let node_key = get_nodes(&registry, &core, &mainloop, &pairs[..]);
     // start with everything muted
     for (node, _) in &node_key {
-        set_mute(node, true);
+        set_mute(&node.proxy, true);
     }
     do_roundtrip(&mainloop, &core);
     println!("{:?}", node_key);
 
     let listener = setup_keyboard_listener();
+    let mut key_states = HashMap::<u32, bool>::new();
+    for (node, _) in &node_key {
+        key_states.insert(node.global_id, false);
+    }
     loop {
         let event = listener.next_event();
-        let (key, mute) = match event {
-            // unmute on press, back to mute on release
-            KeyEvent::Press(key) => (key, false),
-            KeyEvent::Release(key) => (key, true)
-        };
-        if mute && release_delay > 0 {
-            std::thread::sleep(Duration::from_millis(release_delay));
-        }
+        let key = event.keysym;
         let mut change = false;
-        for (node, k) in &node_key {
-            if *k == key {
-                set_mute(node, mute);
-                change = true;
+        for (node, (key_type, k)) in &node_key {
+            if key == *k {
+                if *key_type == KeyType::HOLD {
+                    // unmute on press, back to mute on release
+                    let mute = event.type_ == KeyEventType::RELEASE;
+                    if mute && release_delay > 0 {
+                        std::thread::sleep(Duration::from_millis(release_delay));
+                    }
+                    set_mute(&node.proxy, mute);
+                    change = true;
+                } else if event.type_ == KeyEventType::PRESS { // toggle
+                    let state = key_states.get_mut(&node.global_id).unwrap();
+                    *state = !*state;
+                    set_mute(&node.proxy, *state);
+                    change = true;
+                }
             }
         }
         if change {
@@ -243,7 +285,7 @@ fn set_mute(node: &pw::node::Node, mute: bool) {
     }
 }
 
-fn get_nodes(registry: &Registry, core: &Core, mainloop: &MainLoop, name_key: &[(&str, c_ulong)]) -> Vec<(pw::node::Node, c_ulong)> {
+fn get_nodes(registry: &Registry, core: &Core, mainloop: &MainLoop, name_key: &[(&str, (KeyType, c_ulong))]) -> Vec<(Node, (KeyType, c_ulong))> {
     let mut out = Vec::new();
     for_each_object(registry, core, mainloop, |global| {
         if global.props.is_none() { return false }
@@ -252,7 +294,11 @@ fn get_nodes(registry: &Registry, core: &Core, mainloop: &MainLoop, name_key: &[
             if let Some(name) = props.get("node.name") {
                 if let Some((_, key)) = name_key.iter().find(|(name_in, _)| name == *name_in) {
                     let proxy = registry.bind(global).unwrap();
-                    out.push((proxy, *key))
+                    let node = Node {
+                        global_id: global.id,
+                        proxy
+                    };
+                    out.push((node, *key))
                 }
             }
         }
